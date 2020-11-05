0e3cfe3620d88f1cf9cf702b51852e8672f3fa40
Sihao Sun <sihao.sun1@gmail.com>
2020-11-05 21:58:41 +0100

    reorganize all parameters

---
+++ controlParams.m
@@ -0,0 +1,52 @@
+%% Controller related Parameters
+% Controls
+par.freq = 500; % control frequency
+
+%
+%   (1)<-- b -->(2)
+%      \       / ^
+%       \     /  |
+%       /     \  | l
+%      /       \ v
+%   (4)         (3)
+
+par.fail_id = [3];      % index of the failured propeller
+par.DRF_enable = 1;     % failure of two diagonal rotors?
+par.fail_time = 0.0;    % moment failiure occurs
+
+% drone parameters
+par.b = 0.1150;     % [m]
+par.l = 0.0875;
+par.Ix = 0.0014;    % [kg m^2]
+par.Iy = 0.0013;
+par.Iz = 0.0025;
+par.mass = 0.375;   % [kg]
+par.g = 9.81;
+
+par.k0 = 1.9e-6;    % propeller thrust coefficient
+par.t0 = 1.9e-8;    % torque coefficient
+par.w_max = 1200;   % max / min propeller rotation rates, [rad/s]
+par.w_min = 0;
+
+%% INDI reduced att control
+par.chi = 105;          % output scheduling parameter, [deg].
+par.pos_z_p_gain = 5;   % altitude control pd gains
+par.pos_z_d_gain = 3;
+par.axis_tilt = 0.0;    % primary axis tilting param, 0 ~ 0.2,  
+                        % must be 0 for double rotor failure cases
+
+par.att_p_gain = 200;   % attitude control pd gains 
+par.att_d_gain = 30;
+par.t_indi = 0.02;      % low-pass filter time constant, [s]
+
+% Yaw control
+par.YRC_Kp_r = 5.0;
+par.YRC_Kp_psi = 5.0;
+
+% position control
+par.position_maxAngle = 30/57.3;    % maximum thrust tilt angle [rad]  
+par.position_Kp_pos = [1.5, 1.5, 1.5];  % position control gains
+par.position_maxVel = 10;           % maximum velocity
+par.position_intLim = 5.0; 
+par.position_Ki_vel = [1.0, 1.0, 1.0];  % velocity gains
+par.position_Kp_vel = [2.0, 2.0, 2.0];
---
+++ simParams.m
@@ -0,0 +1,55 @@
+%% Simulation parameters
+
+simVars.freq = 1000;        % simulation frequency
+simVars.aeroModel = 1;      % aerodynamic model 
+                            % 0 = simple aero model, 1 = complex model
+simVars.g = 9.8124;         % gravity in Delft
+simVars.pos0 = [0,0,0];     % initial position, [m]
+simVars.vel0 = [0,0,0];     % initial velocity, [m/s]
+simVars.att0 = [0,0,0];     % initial roll / pitch / yaw, [rad]
+simVars.q0 = eul2quat(flip(simVars.att0),'ZYX');
+simVars.omega0 = [0, 0, 0]; % initial body rate, [rad/s]
+simVars.actuator.w0 = [720; 720; 720; 720];
+                            % initial propeller speeds
+
+% Sensor noise
+add_noise = 1;              % add sensor noise?
+simVars.accNoise = add_noise*50; 
+simVars.rotAccNoise = add_noise*1000;
+
+simVars.sensor.posNoise = add_noise*[1,1,1]*1e-8;
+simVars.sensor.velNoise = add_noise*[1,1,1]*1e-8;
+simVars.sensor.accNoise = add_noise*[1,1,1]*1e-3;
+simVars.sensor.attNoise = add_noise*[1,1,1]*1e-8;
+simVars.sensor.omegaNoise = add_noise*[1,1,1]*2e-4;
+simVars.sensor.wRotorNoise = add_noise*[1,1,1,1]*0;
+
+simVars.sensor.omega_bias = add_noise*[0.0, 0.0, 0.0];
+simVars.sensor.acc_bias = add_noise*[0.0,-0.0,0.0];
+
+% visualization
+simVars.visFreq = 20;   % visualization frequency
+simVars.track = true;   % track the quadrotor for visualization?
+
+% drone related parameters for simulation
+loadedParams = load('Bebop2_guido_parameters.mat');
+simVars.drone.Iv = loadedParams.parameters.Iv;
+simVars.drone.Ip = loadedParams.parameters.Ip;
+simVars.drone.signr = -1;
+simVars.drone.b = loadedParams.parameters.b;
+simVars.drone.l = loadedParams.parameters.l;
+simVars.drone.R = loadedParams.parameters.R;
+simVars.drone.mass = loadedParams.parameters.m;
+% simVars.drone.I = 0.0021;
+
+simVars.actuator.t_w = 1/30;        %  actuator time constant [s]
+simVars.actuator.rateRise = 5e4;    %  actuator rate limit [rad/s^2]
+simVars.actuator.rateFall = -5e4;
+simVars.actuator.wMax = 1200;       %  max/min propeller rotation rates, [rad/s] 
+simVars.actuator.wMin = 0;
+
+% params for simplex aerodynamic model
+simVars.aero.k0 = 1.9035e-6.*ones(1,4); % propeller thrust coefficient
+simVars.aero.t0 = 1.9202951e-8;         % propeller drag coefficient
+simVars.aero.dr = -1.918988e-3;         % quadrotor yaw damping [Nm.s]
+simVars.aero.rotDrag = [1e-4, 1e-4, 1e-4];  % quadrotor rotation damping [Nm.s]
