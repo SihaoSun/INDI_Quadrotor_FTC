b7cd78cfb49144015ebd68a609ef80f74b8af0fd
Sihao Sun <sihao.sun1@gmail.com>
2023-11-17 17:23:14 +0100

    use eul angles propogate dynamics

--- functions/EOMQuat.m
+++ functions/EOMQuat.m
@@ -7,6 +7,9 @@ q = quatnormalize(q);
 
 rotVelB = x(11:13)';
 att = x(14:16);
+phi = att(1); theta = att(2); psi = att(3);
+
+R = eul2rotm([psi, theta, phi]);
 %% Info
 % z-axis points downwards
 % rotations following right hand rule
@@ -14,36 +17,35 @@ att = x(14:16);
 %% Move drone
 % convert forces to earth frame
 bodyForces = [FM.Fx, FM.Fy, FM.Fz];
-forces = quatRot(q, bodyForces);
+forces = R * bodyForces';
 
 % move drone
-acc = [0,0,sim.g] + forces/sim.drone.mass;
+acc = [0,0,sim.g] + forces'/sim.drone.mass;
 
 % rotate drone
 bodyMoments = [FM.Mx - FM.MxGyro, FM.My - FM.MyGyro, FM.Mz - FM.MzGyro];
 
 % info: rotVelB = [p,q,r]
-pr = rotVelB(1);
-qr = rotVelB(2);
-rr = rotVelB(3);
+wx = rotVelB(1);
+wy = rotVelB(2);
+wz = rotVelB(3);
 Ix = sim.drone.Iv(1,1);
 Iy = sim.drone.Iv(2,2);
 Iz = sim.drone.Iv(3,3);
-rotAccB = [(qr*rr*(Iy-Iz) + FM.Mx - FM.MxGyro)/Ix;
-          (pr*rr*(Iz-Ix) + FM.My - FM.MyGyro)/Iy;
-          (pr*qr*(Ix-Iy) + FM.Mz - FM.MzGyro)/Iz];
-rotAccB = awgn(rotAccB',sim.rotAccNoise);
+rotAccB = [(wy*wz*(Iy-Iz) + bodyMoments(1))/Ix;
+           (wx*wz*(Iz-Ix) + bodyMoments(2))/Iy;
+           (wx*wy*(Ix-Iy) + bodyMoments(3))/Iz]';
 
-qDot = 1/2*quatmultiply(q,[0, rotVelB]);
+qw = q(1); qx = q(2); qy = q(3); qz = q(4);
+qDot = [    0.5 * ( -wx*qx - wy*qy - wz*qz ); %qw
+            0.5 * (  wx*qw + wz*qy - wy*qz ); %qx
+            0.5 * (  wy*qw - wz*qx + wx*qz ); %qy
+            0.5 * (  wz*qw + wy*qx - wx*qy )]'; %qz   
 
-%% calc angle such that psi is continuous
-angle = flip(quat2eul(q,'ZYX'));
-%% Euler Test
-phi = att(1); theta = att(2); %psi = att(3);
 att_dot = zeros(1,3);
-att_dot(1) = pr + tan(theta)*(qr*sin(phi)+rr*cos(phi));
-att_dot(2) = qr*cos(phi)-rr*sin(phi);
-att_dot(3) = (qr*sin(phi)+rr*cos(phi))/cos(theta);
+att_dot(1) = wx + tan(theta)*(wy*sin(phi)+wz*cos(phi));
+att_dot(2) = wy*cos(phi)-wz*sin(phi);
+att_dot(3) = (wy*sin(phi)+wz*cos(phi))/cos(theta);
 
 %%
 x_dot = [vel, acc, qDot, rotAccB, att_dot]; % [1,16]
@@ -58,14 +60,14 @@ simBusOut.omegaDot = [0,0,0]; %rotAcc;
 simBusOut.omegaDotB = rotAccB;
 simBusOut.omega = [0,0,0]; %rotVelB; %TODO
 simBusOut.omegaB = rotVelB;
-simBusOut.att = angle;
+simBusOut.att = att';
 
 simBusOut.q = q;
 
 simBusOut.wRotor = wRotor';
 simBusOut.wRotorMeas = wRotor_meas';
 
-simBusOut.forces = forces;
+simBusOut.forces = forces';
 simBusOut.forcesB = bodyForces;
 simBusOut.moments = quatRotInv(q,bodyMoments);
 simBusOut.momentsB = bodyMoments;
